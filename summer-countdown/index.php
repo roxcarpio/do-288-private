815b0a963fd865bb2a63e02b5300e123b6fc97d6
roxcarpio <rocarlo91@gmail.com>
2021-05-26 20:01:00 +0200

    Update index.php

--- summer-countdown/index.php
+++ summer-countdown/index.php
@@ -3,7 +3,7 @@ $d1=strtotime("June 21");
 $d2=ceil(($d1-time())/60/60/24);
 $build_strategy = getenv('BUILD_STRATEGY');
 
-echo "<h2>Summer Countdown! &#127774;</h2>";
+echo "<h2>Summer Countdown!! &#127774;</h2>";
 echo "<p>There are " . $d2 ." days until Summer!</p>";
 if ($build_strategy == "") {
     echo "<p><b>BUILD_STRATEGY</b> is not set up.</p>";
